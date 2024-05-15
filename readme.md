24ba1afb84b67667beca079c04a00b9db64f650e
Nikita O'Keeffe <okeeffe.nikita@gmail.com>
2024-05-15 16:38:01 +1000

    initial commit

---
+++ readme.md
@@ -0,0 +1,54 @@
+# Discord Ticker Bot
+
+A simple bot that displays the price and FDV (Fully Diluted Valuation) of a cryptocurrency on Discord via Activity Status and Nickname.
+
+## Setup
+
+1. Create a `.env` file in the root directory with the following content:
+    ```
+    DISCORD_BOT_TOKEN=
+    TOKEN_CONTRACT_ADDRESS=
+    ```
+
+2. Visit [Discord Developer Portal](https://discord.com/developers/applications) to create a new application and bot.
+
+3. Copy the bot token and paste it into the `DISCORD_BOT_TOKEN` field in the `.env` file.
+
+4. Copy the token contract address from somewhere like Dexscreener.
+
+5. Paste the token contract address into the `TOKEN_CONTRACT_ADDRESS` field in the `.env` file.
+
+6. Invite the bot to your server via the Developer Portal making sure to enable the 'bot' scope and 'nickname' & 'activities' permissions.
+
+## Installation
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/yourusername/discord-ticker-bot.git
+    ```
+
+2. Navigate to the project directory:
+
+    ```bash
+    cd discord-ticker-bot
+    ```
+
+3. Install dependencies:
+
+    ```bash
+    npm install
+    ```
+
+## Usage
+
+1. Start the bot:
+
+    ```bash
+    npm start
+    ```
+
+## Contributing
+
+Contributions are welcome! This is very basic bot and more features could be added in the future. Feel free to submit pull requests or open issues.
+
---
+++ script.js
@@ -0,0 +1,79 @@
+require('dotenv').config();
+const client = require("./client.js");
+const { ActivityType } = require('discord.js');
+const axios = require('axios');
+
+const { DISCORD_BOT_TOKEN, TOKEN_CONTRACT_ADDRESS } = process.env;
+const DEXSCREENER_API_URL = `https://api.dexscreener.com/latest/dex/tokens/${TOKEN_CONTRACT_ADDRESS}`;
+
+const formatFDV = fdv => `${(fdv / 1000000).toFixed(2)}M`;
+
+client.once('ready', () => {
+    console.log('client is ready!');
+    updateBotStatus().then(() => console.log('bot updated'));
+    setInterval(updateBotStatus, 30000);
+});
+
+async function fetchData() {
+    if (!TOKEN_CONTRACT_ADDRESS) {
+        console.error('token contract address not set in .env file');
+        return [];
+    }
+
+    try {
+        console.log('refreshing price...');
+        const response = await axios.get(DEXSCREENER_API_URL);
+        return response.data['pairs'];
+    } catch(error) {
+        console.error('error fetching data: ', error);
+        return [];
+    }
+}
+
+async function setActivity(priceUsd) {
+    try {
+        console.log('USD:', priceUsd);
+        await client.user.setActivity(`USD: $${priceUsd}`, { type: ActivityType['Watching'] });
+    } catch(error) {
+        console.error('error setting price: ', error)
+    }
+}
+
+async function setNickname(fdv, symbol) {
+    console.log('FDV:', fdv);
+    try {
+        const guild = client.guilds.cache.get("1216704396440637440");
+        const botMember = guild.members.me;
+        await botMember.setNickname(`$${symbol} ${formatFDV(fdv)}`);
+    } catch(error) {
+        console.error('error setting fdv: ', error)
+    }
+}
+
+async function updateBotStatus() {
+    const pairs = await fetchData();
+    try {
+        if (pairs !==  null && pairs.length > 0) {
+            const { fdv, priceUsd, baseToken } = pairs[0];
+            const symbol = baseToken['symbol'];
+            await setActivity(priceUsd);
+            await setNickname(fdv, symbol);
+        } else {
+            console.error('error updating bot: token data not found/invalid API request.\nplease check your token address in the .env file.\n');
+            process.exit(1);
+        }
+    } catch(error) {
+        console.error('error updating bot: ', error);
+    }
+}
+
+client.login(DISCORD_BOT_TOKEN)
+    .then(() => console.log('logged in!'))
+    .catch(error => {
+        if (error.code === 'TokenInvalid') {
+            console.error('invalid discord token, double check your .env file.\n');
+            process.exit(1);
+        } else {
+            console.error('error logging in:', error);
+        }
+    });
