182c09c75d06a2462a0f9aad2876072f41e2d28e
Nikita O'Keeffe <okeeffe.nikita@gmail.com>
2024-05-15 17:21:17 +1000

    removed a decimal place in 'M' fdv format

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const DEXSCREENER_API_URL = `https://api.dexscreener.com/latest/dex/tokens/${TOK
 
 const formatFDV = fdv => {
     if (fdv >= 1000000) {
-        return `${(fdv / 1000000).toFixed(2)}M`;
+        return `${(fdv / 1000000).toFixed(1)}M`;
     } else if (fdv >= 1000) {
         return `${(fdv / 1000).toFixed(0)}K`;
     } else {
